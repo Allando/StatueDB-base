0f5979aa07031b05f622fa7b7225178c87b8b81a
jwin74 <jasp0359@edu.easj.dk>
2016-05-01 17:31:12 +0200

    StatueApp Classes Added Added 3 Sample Classes that may be useful

---
+++ StatueApp/SampleCode/SampleObservableCollectionSingleton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using StatueApp.Annotations;
+
+internal class ObservabelCollectionSingleton : INotifyPropertyChanged
+{
+    /// <summary>
+    /// This is a sample Observable Collection Singleton Class.
+    /// Some changes are needed for this to be usable in C# Code
+    /// </summary>
+
+    // Properties
+    public ObservableCollection<object> ObservableCollectionSingleton { get; set; }
+    public static ObservabelCollectionSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new ObservabelCollectionSingleton();
+            }
+            return _instance;
+        }
+    }
+    
+    // Instancers
+    private static ObservabelCollectionSingleton _instance;
+
+    // Constructor
+    private ObservabelCollectionSingleton()
+    {
+        ObservableCollectionSingleton = new ObservableCollection<object>();
+    }
+
+    // Methods
+    public void Add(object newObject)
+    {
+        ObservableCollectionSingleton.Add(newObject);
+    }
+
+    public void Remove(object objectToBeRemoved)
+    {
+        ObservableCollectionSingleton.Remove(objectToBeRemoved);
+    }
+
+    #region PropertyChangedSupport
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
---
+++ StatueApp/SampleCode/SampleSingleton.cs
@@ -0,0 +1,27 @@
+﻿public class Singleton // Class name neeeds to be changed
+{
+    /// <summary>
+    /// This is a sample Singleton Class. It shows the Code for a simple Singleton pattern
+    /// Some changes are needed for this to be usable in C# Code
+    /// </summary>
+
+    private static Singleton _instance; // Should match class name
+
+    private Singleton() { } // Constructor only accesible from inside the class itself
+
+    /// <summary>
+    /// Public Method that return a singleton refence
+    /// </summary>
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
+
