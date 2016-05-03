95d4030b6b2a76c59ff70fed4f8b49d73fff5c28
jwin74 <jasp0359@edu.easj.dk>
2016-05-03 10:32:00 +0200

    StatueApp UnitTest Project UnitTest for StatueApp Added

---
+++ UnitTestApp/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using StatueApp.SampleCode;
+using StatueApp.Model;
+
+namespace UnitTestApp
+{
+    [TestClass]
+    public class UnitTestApp
+    {
+        [TestMethod]
+        public void TestGenericGet()
+        {
+            modelStatue result = new modelStatue();
+
+            result = Facade.GetItemAsync<modelStatue>(5).Result;
+
+            Assert.AreEqual(5,result.Id);
+        }
+    }
+}
