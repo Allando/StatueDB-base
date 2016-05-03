16d1ba47f891ca1266b5f7dbf088e3f2af008b27
atsn <anders_stubberup@hotmail.com>
2016-05-03 10:58:04 +0200

    Har tilføjet nogle observerble collections til singletonen og har bindet alle SelectetItem i CreateStatue2 til nogle properties
    Er gået igang med Opret Statue metoden i handleren

--- StatueApp/Common/StatueSingleton.cs
+++ StatueApp/Common/StatueSingleton.cs
@@ -10,22 +10,25 @@ namespace StatueApp.Common
 {
     public class StatueSingleton
     {
-
+#region Properties
         public modelCulturalValue CulturalValue { get; set; }
         public modelDescription Description { get; set; }
         public modelGPSLocation GpsLocation { get; set; }
         public modelImage Image { get; set; }
         public modelMaterial Material { get; set; }
-        public int Placement { get; set; }
+        public modelPlacement Placement { get; set; }
         public modelStatue Statue { get; set; }
-        public int StatueType { get; set; }
+        public modelStatueType StatueType { get; set; }
         public modelZipcode Zipcode { get; set; }
+#endregion
 
-        public ObservableCollection<modelMaterial> All_Materials { get; } 
+#region Collections
+        public ObservableCollection<modelMaterial> All_Materials { get; }
         public ObservableCollection<string> Materialtypes { get; }
         public ObservableCollection<modelMaterial> Maeterial_By_Type { get; }
-
-
+        public ObservableCollection<modelPlacement> Placements { get; }
+        public ObservableCollection<modelStatueType> StatueTypes; 
+#endregion
 
         #region Singleton
         private static StatueSingleton _instance;
@@ -47,6 +50,8 @@ namespace StatueApp.Common
             All_Materials = new ObservableCollection<modelMaterial>();
             Maeterial_By_Type = new ObservableCollection<modelMaterial>();
             Materialtypes = new ObservableCollection<string>();
+            StatueTypes = new ObservableCollection<modelStatueType>();
+            Placements = new ObservableCollection<modelPlacement>();
         }
     }
 }
--- StatueApp/Handler/handlerStatue.cs
+++ StatueApp/Handler/handlerStatue.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Windows.ApplicationModel.Appointments;
+using Newtonsoft.Json.Linq;
 using StatueApp.Common;
+using StatueApp.Model;
 
 namespace StatueApp.Handler
 {
@@ -33,6 +39,43 @@ namespace StatueApp.Handler
 
             }
         }
+
+        /// <summary>
+        /// Denne Metode Vil Oprette En Statue Og Gemme Den I Databasen
+        /// </summary>
+        /// <returns> Fejlbesked Eller Sysesbesked </returns>
+        public static string Opretstatue()
+        {
+            StatueSingleton Singleton = StatueSingleton.Instance;
+            modelCulturalValueList modelCulturalValueList = new modelCulturalValueList();
+            modelImageList modelImageList = new modelImageList();
+            modelMaterialList materialList = new modelMaterialList();
+            modelPlacementList modelPlacementList = new modelPlacementList();
+            modelStatueTypeList modelStatueTypeList = new modelStatueTypeList();
+
+            // her skal du sette alle statueiderne på listerne
+
+            modelStatueTypeList.FK_StatueType = Singleton.StatueType.Id;
+            modelPlacementList.FK_Placement = Singleton.Placement.Id;
+            materialList.FK_Material = Singleton.Material.Id;
+            modelCulturalValueList.FK_CulturalValue = Singleton.CulturalValue.Id;
+            
+            // jeg laver denne if da det ikke er sikkert at der er et billed
+            if (Singleton.Image != null)
+            {
+                modelImageList.FK_Image = Singleton.Image.Id;
+            }
+
+
+            //Temp Er bare så den kan builde mens jeg laver metoden
+            string temp = "Hej";
+            return temp;
+
+
+        }
+
+
+        
 
 
 
