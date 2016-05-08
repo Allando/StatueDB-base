3ffaa080e2fc8e914425554a5d4a7a61c796ab0f
Thomas Kruse Markussen <thom313c@edu.easj.dk>
2016-05-08 14:31:43 +0200

    Har tilføjet metoder GetStatueType GetStatuePlacement Singleton: StatuetypeSingleton PlacementSingleton

---
+++ StatueApp/Model/StatueTypeSingleton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using StatueApp.Annotations;
+
+namespace StatueApp.Model
+{
+    public class StatueTypeSingleton : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// This is a sample Singleton Class. It shows the Code for a simple Singleton pattern
+        /// Some changes are needed for this to be usable in C# Code
+        /// </summary>
+        public ObservableCollection<modelStatueType> StatueTypes { get; }
+
+        //public static PlacmentSingleton Instance
+        //{
+        //    get
+        //    {
+        //        if (_instance == null)
+        //        {
+        //            _instance = new PlacmentSingleton();
+        //        }
+        //        return _instance;
+        //    }
+        //}
+        public static StatueTypeSingleton Instance => _instance ?? (_instance = new StatueTypeSingleton());
+
+        private static StatueTypeSingleton _instance; // Should match class name
+
+        // Constructor only accesible from inside the class itself
+        private StatueTypeSingleton()
+        {
+            StatueTypes = new ObservableCollection<modelStatueType>();
+        }
+
+        /// <summary>
+        /// Public Method that return a singleton refence
+        /// </summary>
+        public void Add(modelStatueType newStatue)
+        {
+            StatueTypes.Add(newStatue);
+        }
+
+        #region PropertyChangedSupport
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ StatueApp/ViewModel/ViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using StatueApp.Annotations;
+using StatueApp.Facade;
+using StatueApp.Model;
+
+
+namespace StatueApp.ViewModel
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        #region Properties
+        public StatueTypeSingleton StatueTypeSingleton { get; }
+        public PlacmentSingleton PlacementSingleton { get; } 
+        #endregion
+
+        #region Constructor
+        public ViewModel()
+        {
+            //Henter singleton ned så den kan bruges i meotderne
+            StatueTypeSingleton = StatueTypeSingleton.Instance;
+            //Skyder metoden så den bruges
+            GetStatueTypeAsync();
+
+            //Henter singleton ned så den kan bruges i meotderne
+            PlacementSingleton = PlacmentSingleton.Instance;
+            //Skyder metoden så den bruges
+            GetStatuePlacementAsync();
+        } 
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// Henter StatueTyper
+        /// </summary>
+        public async void GetStatueTypeAsync()
+        {
+            var listOfStatueType = await facadeStatue.GetListAsync(new modelStatueType());
+            foreach (var statueType in listOfStatueType)
+            {
+                StatueTypeSingleton.Add(statueType);
+            }
+        }
+        /// <summary>
+        /// Henter StatuePlacering
+        /// </summary>
+        public async void GetStatuePlacementAsync()
+        {
+            var listOfStatuePlacement = await facadeStatue.GetListAsync(new modelPlacement());
+            foreach (var statuePlacement in listOfStatuePlacement)
+            {
+                PlacementSingleton.Add(statuePlacement);
+            }
+        }
+        #endregion
+
+        #region PropertyChangedSupport
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+    }
+}
