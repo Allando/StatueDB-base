38850c9b7136602d248b70527535cf10e80531c1
jwin74 <jasp0359@edu.easj.dk>
2016-05-16 15:05:08 +0200

    StatueApp ViewModel, Model Added Models and ViewModel for Add Damage

---
+++ StatueApp/Model/modelDamageImageList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatueApp.Model
+{
+    public class modelDamageImageList
+    {
+        public int Id { get; private set; }
+        public int FK_Damage { get; set; }
+        public int FK_Image { get; set; }
+
+        public modelDamageImageList(int fkDamage, int fkImage)
+        {
+            FK_Damage = fkDamage;
+            FK_Image = fkImage;
+        }
+    }
+}
---
+++ StatueApp/Model/modelDamageType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatueApp.Model
+{
+    public class modelDamageType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        public modelDamageType(string type)
+        {
+            Type = type;
+        }
+    }
+}
