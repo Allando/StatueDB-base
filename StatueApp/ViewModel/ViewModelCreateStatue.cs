fd53892c5200e7dbf5db8d84f78ac8bc147ee0e5
jwin74 <jasp0359@edu.easj.dk>
2016-05-17 11:27:53 +0200

    StatueApp ViewModel Minor codefixes to ViewModelCreateStatue

--- StatueApp/ViewModel/ViewModelCreateStatue.cs
+++ StatueApp/ViewModel/ViewModelCreateStatue.cs
@@ -19,7 +19,7 @@ namespace StatueApp.ViewModel
         #region Properties
         public static ObservableCollection<modelStatueType> StatueType { get; set; }
         public static ObservableCollection<modelPlacement> StatuePlacement { get; set; }
-        public static ObservableCollection<modelCulturalValue> CulturalValues { get; set; }
+        public static ObservableCollection<modelCulturalValue> CulturalValue { get; set; }
         public static ObservableCollection<modelImage> StatueImage { get; set; }
         public static ObservableCollection<modelMaterial> StatueMaterial { get; set; }
 
@@ -37,7 +37,7 @@ namespace StatueApp.ViewModel
             StatuePlacement = new ObservableCollection<modelPlacement>();
             GetStatuePlacementAsync();
 
-            CulturalValues = new ObservableCollection<modelCulturalValue>();
+            CulturalValue = new ObservableCollection<modelCulturalValue>();
             GetCulturalValueAsync();
 
             StatueImage = new ObservableCollection<modelImage>();
@@ -52,14 +52,14 @@ namespace StatueApp.ViewModel
         /// <summary>
         /// Genererer en Observable Collection af en Bestemt type Materialer 
         /// </summary>
-        /// <param name="c">Materiale Type</param>
+        /// <param name="s">Materiale Type</param>
         /// <returns>ObservableCollection of modelMaterial</returns>
-        public static ObservableCollection<modelMaterial> GetSpecificMaterialList(string c)
+        public static ObservableCollection<modelMaterial> GetSpecificMaterialList(string s)
         {
             var MaterialList = new ObservableCollection<modelMaterial>();
             foreach (var material in StatueMaterial)
             {
-                if (string.Equals(material.MaterialType, c, StringComparison.CurrentCultureIgnoreCase))
+                if (string.Equals(material.MaterialType, s, StringComparison.CurrentCultureIgnoreCase))
                 {
                     MaterialList.Add(material);
                 }
@@ -90,13 +90,15 @@ namespace StatueApp.ViewModel
                 StatuePlacement.Add(statuePlacement);
             }
         }
-
+        /// <summary>
+        /// Henter CulturalValue
+        /// </summary>
         public async void GetCulturalValueAsync()
         {
             var listOfCulturalValue = await facadeStatue.GetListAsync(new modelCulturalValue());
             foreach (var culturalValues in listOfCulturalValue)
             {
-                CulturalValues.Add(culturalValues);
+                CulturalValue.Add(culturalValues);
             }
         }
 
@@ -116,10 +118,11 @@ namespace StatueApp.ViewModel
             {
                 StatueMaterial.Add(statueMaterial);
             }
+
+            // Nødvendigt at kalde herfra da Materiale listen eller ikke er klar
             StatueMaterialStone = GetSpecificMaterialList("s");
             StatueMaterialMetal = GetSpecificMaterialList("m");
             StatueMaterialOther = GetSpecificMaterialList("a");
-
         }
         #endregion
 
