b23dbefaf6c0049e6b2a9b53abf0a1e56cb32d87
jwin74 <jasp0359@edu.easj.dk>
2016-05-17 11:21:10 +0200

    StatueApp ViewModel Implemeted INotifyPropertyChanged in ViewModelAddDamage

--- StatueApp/ViewModel/ViewModelAddDamage.cs
+++ StatueApp/ViewModel/ViewModelAddDamage.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using StatueApp.Annotations;
 using StatueApp.Model;
 
 namespace StatueApp.ViewModel
 {
-    public class ViewModelAddDamage
+    public class ViewModelAddDamage : INotifyPropertyChanged
     {
         public static ObservableCollection<modelDamageType> DamageTypeList;
+
+        #region PropertyChanged Support
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
     }
 }
