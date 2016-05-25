fd85dbf827cd09c201f7806417d8e89a67f9c61c
Schandorph77 <lind6181@edu.easj.dk>
2016-05-25 14:32:48 +0200

    Unit test på ViewModel Statue

--- UnitTestViewModelStatue/UnitTest.cs
+++ UnitTestViewModelStatue/UnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StatueApp.Model;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
 namespace UnitTestViewModelStatue
@@ -6,9 +7,20 @@ namespace UnitTestViewModelStatue
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private modelStatue _statue;
+
+
+        [TestInitialize]
+        public void Before()
         {
+            _statue = new modelStatue(5, "TestStatue", "Elisagaardsvej 5 4000 Roskilde", "4000", DateTime.Now, DateTime.Now); 
+        }
+
+        [TestMethod]
+        public void TestGetStatueMethod()
+        {
+            
         }
     }
 }
