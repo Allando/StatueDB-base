ae0bee9a3f95cdfba053b74a64ddfd5794279929
jwin74 <jasp0359@edu.easj.dk>
2016-05-25 11:42:22 +0200

    StatueApp ViewModel, Handler Started on AddDamage methods

--- StatueApp/Handler/handlerDamage.cs
+++ StatueApp/Handler/handlerDamage.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StatueApp.Common;
+using StatueApp.Facade;
+using StatueApp.Model;
 
 namespace StatueApp.Handler
 {
-    class handlerDamage
+    public class handlerDamage
     {
+        private static async Task<int> GetHighestDamageId()
+        {
+            var damageList = await facadeStatue.GetListAsync(new modelStatue());
+
+            //int max = 0;
+            //foreach (var item in statueList)
+            //    max = Math.Max(max, item.Id);
+            //return max;
+
+            // Løber listen af statuer igennem og finder og retunerer det højeste Id (Sidst tilføjede statue)
+            // Gør det samme som ovenstående Loop
+            return damageList.Select(item => item.Id).Concat(new[] { 0 }).Max();
+        }
+
+
+        public static async Task<string> AddDamage(int statueId)
+        {
+            string statusMsg;
+            var NewDamage = DamageSingleton.Instance;
+
+            try
+            {
+                statusMsg = await Facade.facadeStatue.PostAsync(NewDamage.Damage);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return "Not Implemented";
+        }
     }
 }
--- StatueApp/ViewModel/ViewModelAddDamage.cs
+++ StatueApp/ViewModel/ViewModelAddDamage.cs
@@ -7,13 +7,33 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using StatueApp.Annotations;
+using StatueApp.Common;
+using StatueApp.Facade;
 using StatueApp.Model;
 
 namespace StatueApp.ViewModel
 {
     public class ViewModelAddDamage : INotifyPropertyChanged
     {
-        public static ObservableCollection<modelDamageType> DamageTypeList;
+        public static DamageSingleton NewDamage { get; set; }
+        public static ObservableCollection<modelDamageType> DamageTypes;
+
+        public ViewModelAddDamage()
+        {
+            NewDamage = DamageSingleton.Instance;
+
+            DamageTypes = new ObservableCollection<modelDamageType>();
+            GetDamageTypesAsync();
+        }
+
+        public async void GetDamageTypesAsync()
+        {
+            var listOfDamageTypes = await facadeStatue.GetListAsync(new modelDamageType());
+            foreach (var item in listOfDamageTypes)
+            {
+                DamageTypes.Add(item);
+            }
+        }
 
         #region PropertyChanged Support
         public event PropertyChangedEventHandler PropertyChanged;
